be7fb753244381a5a99c6b48148c0141dce3b466
Nicola Zambello <nzambello@protonmail.com>
2021-08-31 15:57:57 +0200

    fix: empty case handling

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,13 @@
 export function getItemsByPath(items, pathname) {
   let rootPathConfig = null;
-  const itemsByPath = items?.reduce((acc, val) => {
-    if (val.rootPath === '/') {
-      rootPathConfig = val;
-      return acc;
-    }
-    return { ...acc, [val.rootPath]: val };
-  }, {});
+  const itemsByPath =
+    items?.reduce((acc, val) => {
+      if (val.rootPath === '/') {
+        rootPathConfig = val;
+        return acc;
+      }
+      return { ...acc, [val.rootPath]: val };
+    }, {}) ?? {};
   const matchingPaths = Object.keys(itemsByPath)
     .filter((path) => pathname.startsWith(path))
     .sort((a, b) => {
