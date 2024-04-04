061317ccfac4e769033dbf8cedf884b00d8e2ccd
Giulia Ghisini <giulia.ghisini@redturtle.it>
2024-04-04 11:23:53 +0200

    fix: import name

--- src/widget/index.js
+++ src/widget/index.js
@@ -2,12 +2,12 @@ import loadable from '@loadable/component';
 
 export const SubFooterConfigurationForm = loadable(() =>
   import(
-    /* webpackChunkName: "volto-subfooter-manage" */ 'volto-subfooter/widget/SubFooterConfigurationForm'
+    /* webpackChunkName: "volto-subfooter-manage" */ './widget/SubFooterConfigurationForm'
   ),
 );
 
 export const SubFooterConfigurationWidget = loadable(() =>
   import(
-    /* webpackChunkName: "volto-subfooter-manage" */ 'volto-subfooter/widget/SubFooterConfigurationWidget'
+    /* webpackChunkName: "volto-subfooter-manage" */ './widget/SubFooterConfigurationWidget'
   ),
 );
