080a0dda35d146dab5152ff1db12532bc4da9243
Giulia Ghisini <giulia.ghisini@redturtle.it>
2024-06-07 10:31:26 +0200

    fix: match path

--- src/utils.js
+++ src/utils.js
@@ -9,7 +9,7 @@ export function getItemsByPath(items, pathname) {
       return { ...acc, [val.rootPath]: val };
     }, {}) ?? {};
   const matchingPaths = Object.keys(itemsByPath)
-    .filter((path) => pathname.startsWith(path))
+    .filter((path) => pathname === path || pathname.startsWith(`${path}/`))
     .sort((a, b) => {
       if (a.length > b.length) return -1;
       else if (a.length < b.length) return 1;
